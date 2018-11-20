ceda2b5bbfa0ae5fe82dac4d93c468cc3f3472a9
Zamir Johl <johl.z@husky.neu.edu>
2018-11-19 19:50:40 -0500

    Basic book card styling

--- book_list/assets/js/book_card.jsx
+++ book_list/assets/js/book_card.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function BookCard(props) {
     let {book} = props;
     return <div className="book-card">
-        <div className="book-content">
-            <a href={"/books/" + book.id}>
-                <img className="cover-img" src={book.cover_url} alt={book.title}/>
-            </a>
+        <div className="cover-image">
+        <a href={"/books/" + book.id}>
+            <img className="cover" src={book.cover_url} alt={book.title}/>
+        </a>
+        </div>
+        <div className="book-info">
             <h2 className="book-title">{book.title}</h2>
-            <h3 className="book-author">{book.author}</h3>
+            <h4 className="book-author">{book.author}</h4>
         </div>
     </div>;
 }
